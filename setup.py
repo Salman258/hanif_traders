e84ba4ccf6565b8237952cc6098b98a1176466a8
Salman258 <salman.hanif917@gmail.com>
2025-05-25 14:36:41 +0500

    Add setup.py for hanif_traders

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='hanif_traders',
+    version='0.0.1',
+    description='Custom App for Hanif Traders',
+    author='Your Name',
+    author_email='you@example.com',
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True,
+    install_requires=['frappe'],
+)
+
